c73bd0fdc21143ea0786758f84f0d612daf285f7
nesamouzehkesh <n.mouzehkesh@gmail.com>
2018-06-26 12:44:16 +1000

    font face

---
+++ src/components/Details.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Details = (props) => (
+    <h1> Currently in {}: </h1>
+    <div> {} </div>
+    <a>forcast</a>
+    <a>specific date</a>
+)
+
+export default Details;
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,8 +3,8 @@ import { Card } from 'antd';
 
 const Title = () => (
     <div>
-        <h1 style={{ color: '#fff', marginTop: '80px', fontWeight: 'bold' }}>What</h1>
-        <h1 style={{ color: '#fff', fontWeight: 'bold' }}>the Weather?!</h1>
+        <h1 style={{ color: '#fff', marginTop: '80px', fontWeight: 'bold', fontFamily: 'junction-light' }}>What</h1>
+        <h1 style={{ color: '#fff', fontWeight: 'bold', fontFamily: 'junction-light' }}>the Weather?!</h1>
 
     </div>
 )
