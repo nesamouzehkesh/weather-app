b201b50def067a7d99f7884fe9c9c7e936c61d8d
nesamouzehkesh <n.mouzehkesh@gmail.com>
2018-06-27 13:40:57 +1000

    bug fixed

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -10,18 +10,6 @@ const Weather = ({ backHandler, weatherVisible, city, country, windSpeed, temper
                     <span style={{ fontWeight: 'bold', fontFamily: 'junction-light', fontSize: '40px' }}>
                         {city}, {country}
                     </span>
-                    <span style={{ marginLeft: '30px', fontSize: '17px', marginRight: '6px' }}>
-                        <a onClick={() => getWeeklyWeather()}>
-                            <Icon type="bar-chart" style={{ marginRight: '2px', fontSize: 25, color: '#faf714' }} />
-                        </a>
-                        Weekly Forcast
-                    </span>
-                    <span style={{ marginLeft: '30px', fontSize: '17px' }}>
-                        <a>
-                            <Icon type="dot-chart" style={{ marginRight: '2px', fontSize: 25, color: '#faf714' }} />
-                        </a>
-                        Specific Date
-                    </span>
                 </div>
             }
             {
@@ -33,7 +21,21 @@ const Weather = ({ backHandler, weatherVisible, city, country, windSpeed, temper
             {
                 description && <p > Conditions:<span> {description} </span></p>
             }
-            <a onClick={() => backHandler()}> <Icon type="caret-left" style={{ marginTop: '60px', fontSize: 40, color: '#faf714' }} /></a>
+            <div>
+                <span><a onClick={() => backHandler()}> <Icon type="caret-left" style={{ marginTop: '60px', fontSize: 40, color: '#faf714' }} /></a></span>
+                <span style={{ marginLeft: '30px', fontSize: '17px', marginRight: '6px' }}>
+                    <a onClick={() => getWeeklyWeather()}>
+                        <Icon type="bar-chart" style={{ marginRight: '2px', fontSize: 25, color: '#faf714' }} />
+                    </a>
+                    Weekly Forcast
+                    </span>
+                <span style={{ marginLeft: '30px', fontSize: '17px' }}>
+                    <a>
+                        <Icon type="dot-chart" style={{ marginRight: '2px', fontSize: 25, color: '#faf714' }} />
+                    </a>
+                    Specific Date
+                    </span>
+            </div>
         </div>}
     </div>
 )
--- src/components/Weekly.js
+++ src/components/Weekly.js
@@ -4,14 +4,15 @@ const Weekly = ({ showWeekly, weekWeather, weatherVisible }) => (
     <div style={{ display: 'flex', marginTop: '15px' }}>
         {
             weekWeather.map(day => {
-                !weatherVisible && showWeekly &&
+                return (
+                    !weatherVisible && showWeekly &&
                     <div style={{ display: 'flex', flexDirection: 'column' }}>
                         <p>{day.date}</p>
                         <p>{day.min - day.max}</p>
                         <p>{day.precipitation}</p>
                     </div>
-            }
-            )
+                )
+            })
         }
     </div>
 )
