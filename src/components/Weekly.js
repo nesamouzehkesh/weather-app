82493aed3fd07a18c72cbc1c9448b1ed1a770b20
nesamouzehkesh <n.mouzehkesh@gmail.com>
2018-06-27 17:26:29 +1000

    7 days report view done

--- src/components/Weekly.js
+++ src/components/Weekly.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
-const Weekly = ({ showWeekly, weekWeather, weatherVisible }) => (
-    <div style={{ display: 'flex', marginTop: '15px' }}>
-        {
-            weekWeather.map(day => {
-                return (
-                    !weatherVisible && showWeekly &&
-                    <div style={{ display: 'flex', flexDirection: 'column' }}>
-                        <p>{day.date}</p>
-                        <p>{day.min - day.max}</p>
-                        <p>{day.precipitation}</p>
-                    </div>
-                )
-            })
-        }
-    </div>
-)
+const Weekly = ({ showWeekly, weekWeather, weatherVisible }) => {
+    function getDayOfWeek(date) {
+        var dateFormat = new Date(date);
+        var dayOfWeek = dateFormat.getDay();
+        return isNaN(dayOfWeek) ? null : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
+    }
+    return (
+        <div style={{ display: 'flex', marginTop: '15px', flexWrap: 'wrap' }}>
+            {
+                weekWeather.map(day => {
+                    return (
+                        !weatherVisible && showWeekly &&
+                        <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '20px', alignItems: 'center' }}>
+                            <p>{getDayOfWeek(day.date)}, {day.date.split('-')[1]}/ {day.date.split('-')[2]}</p>
+                            <p>min: {Number(day.min.toFixed(1))} - max: {Number(day.max.toFixed(1))}</p>
+                            <p>precipitation: {day.precipitation}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
 
 export default Weekly;
